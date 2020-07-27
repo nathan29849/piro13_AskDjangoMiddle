f18a4958e093cb52c19fbf94231a070004cbfd2a
nathan29849 <gkstjr5731@hufs.ac.kr>
2020-07-28 01:14:16 +0900

    7~8강

--- project_1/accounts/views.py
+++ project_1/accounts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
+from django.contrib.auth import  login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
@@ -11,18 +12,32 @@ from django.contrib.auth.models import User
 #     if request.method == "POST":
 #         form = UserCreationForm(request.POST)
 #         if form.is_valid():
-#             user = form.save()
-#             return redirect(settings.LOGIN_URL)
+#             user = form.save() # DB에 유저의 정보 저장되면서 회원가입이 완료되는 시점.
+#             auth_login(request, user)
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)
 #     else:
 #         form = UserCreationForm()
 #     return render(request, 'accounts/signup.html', {
 #         'form': form,
 #     })
 
-signup = CreateView.as_view(model=User,
-                            form_class=UserCreationForm,
-                            success_url=settings.LOGIN_URL,
-                            template_name='accounts/signup.html')
+
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect('profile')
+
+signup = SignupView.as_view()
 
 @login_required   # 로그아웃 상황일 때 settings.LOGIN_URL로 이동시켜준다.
 def profile(request):
